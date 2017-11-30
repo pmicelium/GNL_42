33c9adaefea2ca26e764323324110707df396c3a
Pierre MICELI <pmiceli@e1r7p18.42.fr>
2017-11-30 20:59:00 +0100

    lol

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pmiceli <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 10:46:55 by pmiceli           #+#    #+#             */
-/*   Updated: 2017/11/30 19:59:08 by pmiceli          ###   ########.fr       */
+/*   Updated: 2017/11/30 20:58:49 by pmiceli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,5 @@
 
 int get_next_line(int fd, char **line)
 {
-	write (1, "FUCK THIS SHIT I'AM OUT\n", 24);
-	return (0);
+	int		fd[OPEN_MAX];
 }
